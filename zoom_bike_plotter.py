0c4b0e26b4ae0b59dfd2d6bb5513f134024b93ba
hminsky1 <33550309+hminsky1@users.noreply.github.com>
2023-06-29 09:51:59 -0400

    Update zoom_bike_plotter.py
    Reset last test

--- zoom_bike_plotter.py
+++ zoom_bike_plotter.py
@@ -9,16 +9,14 @@ import re
 
 def geocode(address):
     # this gets the longitude and latitude of the address
-if ', Brooklyn' not in address:
-        print('Make sure to put in an address in Brooklyn. It should be formated like 123 Main Street, Brooklyn.')
-    else:
-        params = { 'format'        :'json',
-                   'addressdetails': 1,
-                   'q'             : address}
-        headers = { 'user-agent'   : 'TDI' }   #  Need to supply a user agent other than the default provided
-                                               #  by requests for the API to accept the query.
-        return requests.get('http://nominatim.openstreetmap.org/search',
-                            params=params, headers=headers)
+
+    params = { 'format'        :'json',
+               'addressdetails': 1,
+               'q'             : address}
+    headers = { 'user-agent'   : 'TDI' }   #  Need to supply a user agent other than the default provided
+                                           #  by requests for the API to accept the query.
+    return requests.get('http://nominatim.openstreetmap.org/search',
+                        params=params, headers=headers)
 
 
 def get_address_lat_long(address):
